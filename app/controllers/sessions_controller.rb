86f97e4c10b54aecb4242d43a2c499aff917d446
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-08-29 16:26:50 -0700

    Test error flash persistence

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,11 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+  end
+
+  def destroy
   end
 end
